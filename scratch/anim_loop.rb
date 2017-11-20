00e7700459251a6435ad698080855763ea11c787
nichol alexander <nichol.alexander@gmail.com>
2017-11-20 13:17:43 -0500

    anim loop with click counter and test render method.

--- scratch/anim_loop.rb
+++ scratch/anim_loop.rb
@@ -1,9 +1,33 @@
 require 'pry'
 
+# require files
+
+# set up game classes
+
+# start
 Shoes.app do
-  counter = 0
+
+  @counter = 0
+  @clicks = 0
+
+  def render
+    @para.text = "this is some info: #{@counter+=1}"
+    @clicker_para.text = "this is how many clicks you have: #{@clicks}"
+  end
+
+  # set up shoes windows
+
   @para = para "this is some info: "
+  @clicker_para = para "this is how many clicks you have: "
+  
+  @clicker_button = button do
+    @clicks +=1
+  end
+
+  # game loop
   @anim = animate 40 do
-    @para.text = "this is some info: #{counter+=1}"
+    render
   end
 end
+
+
